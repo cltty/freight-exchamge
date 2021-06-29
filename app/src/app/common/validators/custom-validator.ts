1514108a20bc248fac87757733a34a9e2f6c8c8e
cltty <claudiu.timofte@protonmail.com>
2021-06-29 12:08:10 +0300

    final version

--- app/src/app/common/validators/custom-validator.ts
+++ app/src/app/common/validators/custom-validator.ts
@@ -3,13 +3,6 @@ import { postcodeValidator, postcodeValidatorExistsForCountry } from 'postcode-v
 import validateVat, {CountryCodes, ViesValidationResponse} from 'validate-vat-ts';
 
 export class CustomValidators {
-    // public static validatePasswordsEquality(controlName: string, matchingControlName: string): ValidatorFn {
-    //     return (control: AbstractControl): { [key: string]: boolean } | null => {
-    //         return (control.value && control.value.match(/^.*?(?=[\^#%&$\*:.;!<>\?/\{\|\}]).*$/)) 
-    //             ? { invalidCharacters: true } : null;  
-    //     };
-    // }
-
     public static forbiddenCharacters(): ValidatorFn {
         return (control: AbstractControl): { [key: string]: boolean } | null => {
             return (control.value && control.value.match(/^.*?(?=[\^#%&$\*:.;!<>\?/\{\|\}]).*$/)) 
@@ -37,39 +30,4 @@ export class CustomValidators {
                 ? { invalidPhoneNumber: true } : null;  
         };
     }
-
-    // public static vatValidator(): ValidatorFn {
-    //     const consumer = async () => {
-    //         try {
-    //           const validationInfo: ViesValidationResponse = await validateVat(CountryCodes.Germany, "12323");
-    //         } catch (e) {
-    //           console.log(e);
-    //         }
-    //       }
-
-
-    //     return (control: AbstractControl): { [key: string]: boolean } | null => {
-    //         return (control.value.length < 10 || control.value.length > 12) 
-    //             ? { invalidPhoneNumber: true } : null;  
-    //     };
-    // }
-
-//   public static validateYear(year: number): ValidatorFn {
-//     return (control: AbstractControl): { [key: string]: boolean } | null => {
-//       if (control.value && !CustomValidators.dateInAccountantYear(control.value, year)) {
-//         return { invalidYear: true };
-//       }
-//       return null;
-//     };
-//   }
-
-//   private static dateInAccountantYear(selectedDate: string, year: number): boolean {
-//     const startDateOfYear = new Date('04/06/' + (year - 1));
-//     const endDateOfYear = new Date('04/06/' + year);
-//     const selectedDateofYear = new Date(selectedDate);
-//     return (
-//       selectedDateofYear.getTime() <= endDateOfYear.getTime() &&
-//       selectedDateofYear.getTime() >= startDateOfYear.getTime()
-//     );
-//   }
 }
